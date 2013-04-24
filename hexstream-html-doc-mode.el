f4f888e22aae9381a440e9ce0a82bbb1e8f90946
Jean-Philippe Paradis <hexstream@gmail.com>
2013-04-24 02:38:37 -0400

    Implement two first trivial commands.

--- hexstream-html-doc-mode.el
+++ hexstream-html-doc-mode.el
@@ -1,2 +1,19 @@
-(define-derived-mode hexstream-html-doc html-mode "Hexstream HTML Documentation"
-  (define-prefix-command 'hexstream-html-doc-ctl-c))
+(define-derived-mode hexstream-html-doc-mode html-mode "Hexstream HTML Documentation"
+  (let ((map hexstream-html-doc-mode-map))
+    (define-key map (kbd "C-c w v") 'hexstream-html-doc-wrap-variable)
+    (define-key map (kbd "C-c i t") 'hexstream-html-doc-insert-tag)))
+
+
+(defun hexstream-html-doc-wrap-variable ()
+  (interactive "*@")
+  (save-excursion
+    (backward-sexp)
+    (insert "<var>")
+    (forward-sexp)
+    (insert "</var>")))
+
+(defun hexstream-html-doc-insert-tag (name)
+  (interactive "*@sInsert tag: ")
+  (insert "<" name ">")
+  (save-excursion
+    (insert "</" name ">")))
