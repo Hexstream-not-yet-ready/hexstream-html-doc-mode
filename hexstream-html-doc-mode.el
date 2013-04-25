5ee85478bb3603eb942496c7860007235917a01a
Jean-Philippe Paradis <hexstream@gmail.com>
2013-04-25 02:50:30 -0400

    Start "smarter" concept for variable function. Does not work yet.

--- hexstream-html-doc-mode.el
+++ hexstream-html-doc-mode.el
@@ -1,15 +1,26 @@
 (define-derived-mode hexstream-html-doc-mode html-mode "Hexstream HTML Documentation"
   (let ((map hexstream-html-doc-mode-map))
-    (define-key map (kbd "C-c w v") 'hexstream-html-doc-wrap-variable)
+    (define-key map (kbd "C-c v") 'hexstream-html-doc-variable)
     (define-key map (kbd "C-c i t") 'hexstream-html-doc-insert-tag)))
 
-
-(defun hexstream-html-doc-wrap-variable ()
-  (interactive "*@")
+;; Doesn't yet work...
+(defun hexstream-html-doc-variable (region-min region-max)
+  (interactive
+   (if (use-region-p)
+       (list (region-beginning) (region-end))
+     ;; Won't work as intended if point is exactly at the beginning or end of s-expression.
+     ;; I don't yet know how to easily determine if point is on a s-expression
+     ;; and if so, what its boundaries are.
+     (list (save-excursion
+             (backward-sexp)
+             (point))
+           (save-excursion
+             (forward-sexp)
+             (point)))))
   (save-excursion
-    (backward-sexp)
+    (goto-char region-min)
     (insert "<var>")
-    (forward-sexp)
+    (goto-char region-max)
     (insert "</var>")))
 
 (defun hexstream-html-doc-insert-tag (name)
