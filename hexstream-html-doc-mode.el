e20abfe0c90f10b2df0a305b8b84f7db9a753cf6
Jean-Philippe Paradis <hexstream@gmail.com>
2013-04-26 02:02:15 -0400

    Mostly usable tag insertion and wrapping.

--- hexstream-html-doc-mode.el
+++ hexstream-html-doc-mode.el
@@ -1,30 +1,44 @@
 (define-derived-mode hexstream-html-doc-mode html-mode "Hexstream HTML Documentation"
   (let ((map hexstream-html-doc-mode-map))
-    (define-key map (kbd "C-c v") 'hexstream-html-doc-variable)
-    (define-key map (kbd "C-c i t") 'hexstream-html-doc-insert-tag)))
+    (define-key map (kbd "C-c t") 'hexstream-html-doc-tag)
+    (define-key map (kbd "C-c v") 'hexstream-html-doc-variable)))
 
-;; Doesn't yet work...
-(defun hexstream-html-doc-variable (region-min region-max)
-  (interactive
-   (if (use-region-p)
-       (list (region-beginning) (region-end))
-     ;; Won't work as intended if point is exactly at the beginning or end of s-expression.
-     ;; I don't yet know how to easily determine if point is on a s-expression
-     ;; and if so, what its boundaries are.
-     (list (save-excursion
-             (backward-sexp)
-             (point))
-           (save-excursion
-             (forward-sexp)
-             (point)))))
-  (save-excursion
-    (goto-char region-min)
-    (insert "<var>")
-    (goto-char region-max)
-    (insert "</var>")))
+(defun hexstream-html-doc-wrap (region-min region-max insert-before insert-after)
+  (interactive (hexstream-html-doc-suitable-region))
+  (cond (region-min
+         (save-excursion
+           (goto-char region-max)
+           (insert insert-after)
+           (goto-char region-min)
+           (insert insert-before)))
+        ;; Todo: smarter handling
+        (t (hexstream-html-doc-wrap (point) (point) insert-before insert-after)
+           (goto-char (+ (point) (length insert-before))))))
 
-(defun hexstream-html-doc-insert-tag (name)
+
+(defun hexstream-html-doc-tag (name)
   (interactive "*@sInsert tag: ")
-  (insert "<" name ">")
-  (save-excursion
-    (insert "</" name ">")))
+  (let (region-min region-max)
+    ;; I don't know if it's possible to mix an interactive string spec and code...
+    (let ((region (hexstream-html-doc-suitable-region)))
+      (setq region-min (first region)
+            region-max (second region)))
+    (hexstream-html-doc-wrap region-min region-max
+                             (concat  "<" name ">") (concat  "</" name ">"))))
+
+(defun hexstream-html-doc-variable (region-min region-max)
+  (interactive (hexstream-html-doc-suitable-region))
+  (hexstream-html-doc-tag "var"))
+
+
+(defun hexstream-html-doc-suitable-region ()
+  (if (use-region-p)
+      (list (region-beginning) (region-end))
+    (if (char-equal (char-syntax (char-after)) ?\s)
+        (list nil nil)
+      (list (save-excursion
+              (skip-syntax-backward "w_")
+              (point))
+            (save-excursion
+              (skip-syntax-forward "w_")
+              (point))))))
